b19d8a4f37a4141c8708caba88e0826af75c8977
Suganth <suganths1999@gmail.com>
2020-04-26 11:38:46 +0530

    bcrypt

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const express=require("express");
 const bodyparser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
-const md5=require("md5");
+const bcrypt=require("bcrypt");
+const saltRounds = 10;
+
 
 const app=express();
+
+
 
 app.use(bodyparser.urlencoded({extended:true}));
 
@@ -43,36 +47,48 @@ app.get("/register",function(req,res){
 
 
 app.post("/register",function(req,res){
-  const newUser = new User({
-    email:req.body.username,
-    password:md5(req.body.password)
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email:req.body.username,
+      password:hash
+    });
+    newUser.save(function(err){
+      if(!err)
+      {
+        res.render("secrets");
+      }
+      else{
+        console.log(err);
+      }
+    });
   });
-  newUser.save(function(err){
-    if(!err)
-    {
-      res.render("secrets");
-    }
-    else{
-      console.log(err);
-    }
-  });
+
 });
+
+
 
 
 app.post("/login",function(req,res){
 
-  User.findOne({email:req.body.username},function(err,foundUser){
+  const username=req.body.username;
+  const password=req.body.password;
+
+  User.findOne({email:username},function(err,foundUser){
     if(err){
       console.log(err);
     }
     else{
-      console.log(foundUser);
-      if(foundUser.password===md5(req.body.password)){
-        res.render("secrets");
+      bcrypt.compare(password, foundUser.password, function(err, result) {
+        if(result==true){
+          res.render("secrets");
+        }
+      });
+
       }
-    }
+    });
   });
-});
+
 
 
 app.listen(3000,function(){
