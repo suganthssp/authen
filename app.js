08987cec6b6e652da3fc5cc8b811738084fa914f
Suganth <suganths1999@gmail.com>
2020-04-26 11:12:41 +0530

    md5

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express=require("express");
 const bodyparser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
-const encrypt=require("mongoose-encryption");
+const md5=require("md5");
+
 const app=express();
 
 app.use(bodyparser.urlencoded({extended:true}));
@@ -22,10 +23,6 @@ const userSchema=new mongoose.Schema({
   email:String,
   password:String
 });
-
-
-
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
 
 
 const User=new mongoose.model("User",userSchema);
@@ -48,7 +45,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
   const newUser = new User({
     email:req.body.username,
-    password:req.body.password
+    password:md5(req.body.password)
   });
   newUser.save(function(err){
     if(!err)
@@ -70,7 +67,7 @@ app.post("/login",function(req,res){
     }
     else{
       console.log(foundUser);
-      if(foundUser.password===req.body.password){
+      if(foundUser.password===md5(req.body.password)){
         res.render("secrets");
       }
     }
